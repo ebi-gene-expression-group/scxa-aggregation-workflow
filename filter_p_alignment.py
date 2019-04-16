e49e02a27e59e25f1996398b59ed31f3edcdf094
Etienne92 <etienne.sollier@polytechnique.edu>
2019-04-16 15:27:01 +0100

    Corrected typo in filtering script

--- filter_p_alignment.py
+++ filter_p_alignment.py
@@ -3,7 +3,7 @@ import json
 import os
 
 kallistofolder = sys.argv[1]
-with open("kallisto_results_filtered","w") as outfile:
+with open("kallisto_results_filtered.txt","w") as outfile:
     for runID in os.listdir(kallistofolder):
         with open(kallistofolder+"/"+runID + "/run_info.json","r") as run_info_file:
             run_info = json.load(run_info_file)
