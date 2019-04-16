7ea028669a89356f940e0b64da899776ea84fac4
Etienne92 <etienne.sollier@polytechnique.edu>
2019-04-16 15:17:29 +0100

    Added python script used for filtering.

---
+++ filter_p_alignment.py
@@ -0,0 +1,12 @@
+import sys
+import json
+import os
+
+kallistofolder = sys.argv[1]
+with open("kallisto_results_filtered","w") as outfile:
+    for runID in os.listdir(kallistofolder):
+        with open(kallistofolder+"/"+runID + "/run_info.json","r") as run_info_file:
+            run_info = json.load(run_info_file)
+            p_alignment = run_info["p_pseudoaligned"]
+            if p_alignment > 25.0:
+                outfile.write(kallistofolder + "/" + runID + "/abundance.h5\n")
